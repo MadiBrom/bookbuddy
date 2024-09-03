09e07e4b9ca64ab9f5120c15466137a35c3ffb9d
Madi Bromfield <madibromfield215@gmail.com>
2024-09-02 19:52:17 -0500

    working on it

--- src/pages/SingleBook.jsx
+++ src/pages/SingleBook.jsx
@@ -26,39 +26,36 @@ export default function SingleBook({ token }) {
     <>
       {book && (
         <div className="single-main">
-          <div id="status-contain">
+          <main className="single-book" key={book.id}>
+            <section>
+              <div>
+                <img className="cover" src={book.coverimage} alt={book.title} />
+                <h3>by {book.author}</h3>{" "}
+              </div>
+              <div>
+                <p>{book.description}</p>
+              </div>
+            </section>
             <div className="availability-status">
+              <h2>{book.title}</h2>
               {book.available ? (
                 <p className="green-text">This book is available.</p>
               ) : (
                 <p className="red-text">This book is currently unavailable.</p>
               )}
             </div>
-            <main className="single-book" key={book.id}>
-              <div>
-                <h2>{book.title}</h2>{" "}
-                <img className="cover" src={book.coverimage} alt={book.title} />
-                <h3>by {book.author}</h3>{" "}
-              </div>
-              <div>
-                <h4>Description</h4>
-                <p>{book.description}</p>
-              </div>
-              {/* Availability Information */}
-              {/* Checkout Button and Status */}
-              {token && book.available && !isCheckedOut && (
-                <button className="checkout-button" onClick={handleCheckout}>
-                  Check out
-                </button>
-              )}
-              {isCheckedOut && <p className="red-text">Checked out</p>}
-              {!token && book.available && (
-                <p className="red-text">
-                  Please log in or register to check out this title.
-                </p>
-              )}
-            </main>
-          </div>
+            {token && book.available && !isCheckedOut && (
+              <button className="checkout-button" onClick={handleCheckout}>
+                Check out
+              </button>
+            )}
+            {isCheckedOut && <p className="red-text">Checked out</p>}
+            {!token && book.available && (
+              <p className="red-text">
+                Please log in or register to check out this title.
+              </p>
+            )}
+          </main>
         </div>
       )}
     </>
